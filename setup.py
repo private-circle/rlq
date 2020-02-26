6792df2ea46d91b8b2aef6df8793aa24fc7a3a8f
Parth Joshi <parth@privatecircle.co>
2020-02-26 18:37:13 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+with open(os.path.join('rlq', 'VERSION'), 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name='rlq',
+    version=version,
+    author='Parth Joshi',
+    author_email='parthjoshi.iitm@gmail.com',
+    license='GNU GPL v3',
+    packages=setuptools.find_packages(),
+    description='A wrapper around Arelle to query XBRL instances using SQL-like queries',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/parthjoshi2007/rlq',
+    dependency_links=['git+https://github.com/Arelle/Arelle.git#egg=Arelle-1.0.0'],
+    install_requires=['Arelle==1.0.0'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ]
+)
